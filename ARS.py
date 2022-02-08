7a12bacec2223ee2358ac368a8f06e4a460e352e
Chillwind132 <99220831+Chillwind132@users.noreply.github.com>
2022-02-07 20:55:07 -0500

    v1.01

--- ARS.py
+++ ARS.py
@@ -1,9 +1,29 @@
 
+
 from pathlib import Path
 from dirsync import sync
 import os.path
-source_path = input (r"Enter the sync source (folder):").strip()
-#source_path.strip()
+import json
+import pathlib
+
+#Check for Config file 
+currentdir = Path(__file__).parent.absolute()
+
+v = str(currentdir)
+
+a = '\config.json'
+c = v + a
+print(c)
+
+if os.path.exists(c):
+     print("Config found")
+else:
+    print("Config NOT found")
+
+
+
+
+source_path = input (r"Enter the sync SOURCE (folder):").strip()
 print(source_path)
 
 my_file = Path(source_path)
@@ -14,19 +34,47 @@ else:
 
     isdir = os.path.isdir(source_path)
     while isdir == False:
-        source_path = input (r"Enter the sync source (folder):").strip()
+        source_path = input (r"Enter the sync SOURCE (folder):").strip()
         isdir = os.path.isdir(source_path)
+        my_file = Path(source_path)
         if my_file.is_dir():
             print("Dir exists")
+        else:
+            print("Dir does not exist, try again")
 
 
 
-target_path = input (r"Enter the sync target (folder):").strip()
+target_path = input (r"Enter the sync TARGET (folder):").strip()
 print(target_path)
 
+my_file = Path(target_path)
+if my_file.is_dir():
+    print("Dir exists")
+else:
+    print("Dir does not exist, try again")
+
+    isdir = os.path.isdir(target_path)
+    while isdir == False:
+        target_path = input (r"Enter the sync TARGET (folder):").strip()
+        isdir = os.path.isdir(target_path)
+        my_file = Path(target_path)
+        if my_file.is_dir():
+            print("Dir exists")
+        else:
+            print("Dir does not exist, try again")
 
 #source_path = 'C:\Users\Mike\Documents\Source_Copy'
 #target_path = 'C:\Users\Mike\Documents\Dest_Copy'
 
 sync(source_path,target_path,'sync', twoway=True, create=True)
+
+op_conf = {
+    r"source_path" : source_path,
+    r"target_path" : target_path,
+}
+
+with open("config.json", "w") as jsonfile:
+    json.dump(op_conf, jsonfile)
+
+
 
