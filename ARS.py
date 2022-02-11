cb296387b51e09b5f6305607af531d4d3ac31c72
Chillwind132 <99220831+Chillwind132@users.noreply.github.com>
2022-02-10 19:55:17 -0500

    QOL Changes

--- ARS.py
+++ ARS.py
@@ -20,7 +20,6 @@ def check_if_config_1stp():
             src = config_loaded["source_path"]
     return src
 
-
 def populate_data():
     mypath = r'C:\Users\Mike\Documents\Dest_Copy'
     nameSet=set()
@@ -34,8 +33,6 @@ def populate_data():
         stat=os.stat(os.path.join(mypath, name))
         time=os.path.getmtime(os.path.join(mypath, name))
         size = os.path.getsize(os.path.join(mypath, name))
-        #size=stat.ST_SIZE If you add this, you will be able to detect file size changes as well.
-        #Also consider using ST_MTIME to detect last time modified
         retrievedSet.add((name, time, size))
 
 
@@ -69,35 +66,47 @@ def check_dir_change():
         file_history_loaded = json.load(f)
 
     decodedSet = jsonpickle.decode(file_history_loaded)
-    #print('Decoded set:', decodedSet)
+    #print('Decoded set:', decodedSet)   
 
     #mypath=Path(__file__).parent.absolute()
-    mypath = r'C:\Users\Mike\Documents\Dest_Copy'
+    mypath = r'C:\Users\Mike\Documents\Source_Copy'
 
     nameSet=set()
     for file in os.listdir(mypath):
         fullpath=os.path.join(mypath, file)
-        if os.path.isfile(fullpath):
+        
+        if  os.path.isfile(fullpath):
             nameSet.add(file)
+        else:
+            os.path.isdir(fullpath)
+            nameSet.add(file)
+
+   
 
     retrievedSet=set()
     for name in nameSet:
         stat=os.stat(os.path.join(mypath, name))
         time=os.path.getmtime(os.path.join(mypath, name))
-        size = os.path.getsize(os.path.join(mypath, name))
-        #size=stat.ST_SIZE If you add this, you will be able to detect file size changes as well.
-        #Also consider using ST_MTIME to detect last time modified
-        retrievedSet.add((name, time, size))
-
-
+        size=os.path.getsize(os.path.join(mypath, name)) 
+        retrievedSet.add((name, time, size)) 
+        
 
     if decodedSet != 0:
         newSet=retrievedSet-decodedSet
+        deletedSet = decodedSet-retrievedSet
         print('NewSet:', newSet)
+        print('DeletedSet:', deletedSet)
 
-        isEmpty = (len(newSet) == 0)
+        isEmpty_1 = (len(newSet) == 0)
+        isEmpty_2 = (len(deletedSet) == 0)
 
-        if isEmpty:
+        if isEmpty_1 == False or isEmpty_2 == False:
+            isEmpty = False
+        else:
+            isEmpty = True
+
+
+        if isEmpty == True:
             print("No File Changes")
         else:
             print("File Changes")
@@ -110,14 +119,10 @@ def check_dir_change():
         with open("data.json", "w") as jsonfile:
            json.dump(sampleJson, jsonfile)
 
+
         return isEmpty
     else:
         newSet = set()
-    
-    
-        
-        
-
     
 
 
@@ -159,7 +164,12 @@ def user_input():
                 print("Dir exists")
             else:
                 print("Dir does not exist, try again")
+
+    
+    
     return source_path, target_path
+    
+
 #Save current config to JSON
 def config_save(s_path, t_path, auto_flag):
     op_conf = {
@@ -170,6 +180,9 @@ def config_save(s_path, t_path, auto_flag):
 
     with open("config.json", "w") as jsonfile:
         json.dump(op_conf, jsonfile)
+
+
+
 #load config from JSON
 def load_config():
     with open('config.json', 'r') as f:
@@ -189,12 +202,7 @@ def check_auto_flag():
             config_loaded = json.load(f)
             flag = config_loaded["Automation Flag"]
         return flag
-    else:
-        config_save('0','0','0')
-
-            
     
-
 
 #Check for Config file 
 flag = check_auto_flag()
@@ -208,15 +216,16 @@ if flag == '1':
     target_path = result[1]
     
     dir_flag = check_dir_change()
-    if dir_flag == True:
+    if dir_flag == False:
         print('File Changes detected, syncing...')
         time.sleep(1)
-    elif dir_flag == False:
+        sync(source_path, target_path, 'sync', create=True)
+    elif dir_flag == True:
         print('No File Changes detected since last scan')
     else:
         print('Unknown exception')
 
-    sync(source_path,target_path,'sync', twoway=True, create=True)
+    
     quit()
 
 
@@ -235,12 +244,15 @@ if os.path.exists(c):
 
     if check_if_config_1stp() == '0':
         print("First time setup mode")
+        dest_array = user_input()
+
+        config_save(dest_array[0], dest_array[1], '0') # SAVE THE S AND T VALUES INTO CONFIG.JSON 
 
     print("Config found")
-    verification = input ("Would you like to load the config and run the sync? Type y/n\n").strip()
+    verification = input ("Would you like to load the config and run the sync now? Or enter the configuration menu? Type y to sync, n to enter menu\n").strip()
     while verification != 'y' and verification != 'n':
         print('Invalid input')
-        verification = input ("Would you like to load the config? Type y/n\n").strip()
+        verification = input("Type y to sync, n to enter menu\n").strip()
 
     if verification == 'y':
 
@@ -267,13 +279,16 @@ else:
 #source_path = 'C:\Users\Mike\Documents\Source_Copy'
 #target_path = 'C:\Users\Mike\Documents\Dest_Copy'
 
+
 sync(source_path,target_path,'sync', twoway=True, create=True)
 
-if verification == 'n':
-    verification_2 = input ("Would you like to save the config? Type y/n\n").strip()
+
+
+verification_2 = input("Would you like to save the config and set up automation mode? Type y/n\n").strip()
+if verification_2 == 'y':
     while verification_2 != 'y' and verification_2 != 'n':
         print('Invalid input')
-        verification_2 = input ("Would you like to save the config? Type y/n\n").strip()
+        verification_2 = input("Would you like to save the config and set up automation mode? Type y/n\n").strip()
 
     if verification_2 == 'y':
         config_save(source_path, target_path, '0')
@@ -286,7 +301,16 @@ if verification == 'n':
             config_save(source_path, target_path, '1')
             print("Automation mode enabled and saved to config.json.")
             print("You can disable it by setting auto_flag property back to '0' ")
-            
+        else:
+            print("Exiting...")
+    elif verification_2 == 'n':
+        print('Config not saved, automation mode can not function without config.json.')
+        print('Exiting the script...')
+
+       
+        
+
+
 
         
 
